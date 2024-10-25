6f0da9bfad1567cff9d882fa9c080835547b0d70
SimpleGame <107101075+MikhailVladimirovi4@users.noreply.github.com>
2024-10-25 17:48:15 +0300

    doneOpenModal

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/headers/header";
 import ToolsBar from "./components/headers/toolsBar";
 import Table from "./components/tables/table";
 import Modal from "./components/modal/modal";
+import Button from "./components/buttons/button";
 
 export default function App() {
   const [onModal, setOnModal] = useState(false);
@@ -21,10 +22,12 @@ export default function App() {
         <main>
           {onModal && (
             <Modal open={onModal}>
-              xxxxxxxxxxxxxxxxxxxxxfsffffffsdfdsdssdf
+              <p>xxxxxxxxxxxxxxxxxxxxxfsffffffsdfdsdssdf</p>
+              <Button onClick={() => setOnModal(false)}>Выполнить</Button>
+              <Button onClick={() => setOnModal(false)}>Отмена</Button>
             </Modal>
           )}
-          <Table onClick={OpenModal}/>
+          <Table onClick={OpenModal} />
         </main>
         <footer className="footer">Автор: Михаил Владимирович Бибиков</footer>
       </div>
